ac1aa74d40022b1ff308273899594f05df3d5d50
Abhishek Pathak <pathakabhishek0003@gmail.com>
2018-07-16 13:06:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ function whisper(word){
 function logShout(word){
   console.log(word.toUpperCase)
 }
+function logWhisper(word){
+  console.log(word.LowerCase)
+}
+
+function sayHiToGrandma(word){
+  if(word.toLowerCase === word){
+    return "I can't hear you!"
+  }
+}
+
+
