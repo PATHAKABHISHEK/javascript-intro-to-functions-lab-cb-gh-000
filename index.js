d65100ee014cf3194ac231b1caae98966ca188de
Abhishek Pathak <pathakabhishek0003@gmail.com>
2018-07-16 13:12:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,27 @@ function shout(string){
   return string.toUpperCase
 }
 
+function whisper(word){
+  return word.toLowerCase
+}
+
+function logShout(word){
+  console.log(word.toUpperCase)
+}
+function logWhisper(word){
+  console.log(word.LowerCase)
+}
+
+function sayHiToGrandma(word){
+  if(word.toLowerCase === word){
+    return "I can't hear you!"
+  }
+  else if(word === "I love you, Grandma"){
+    return "I love you,too"
+  }
+  else if(word.toUpperCase === word){
+    return "YES INDEED!"
+  }
+}
+
+
